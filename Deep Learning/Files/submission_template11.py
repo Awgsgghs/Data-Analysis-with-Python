7ba6661d70f8ce0df46cb2bc6da81b5cc5d8127c
Awgsgghs <zedd75@mail.ru>
2025-12-05 23:30:44 +0300

    Update submission_template11.py
    123

--- Deep Learning/Files/submission_template11.py
+++ Deep Learning/Files/submission_template11.py
@@ -5,24 +5,29 @@ from torch.nn import functional as F
 
 def encoder_block(in_channels, out_channels, kernel_size, padding):
     '''
-    блок, который принимает на вход карты активации с количеством каналов in_channels, 
+    блок, который принимает на вход карты активации с количеством каналов in_channels,
     и выдает на выход карты активации с количеством каналов out_channels
     kernel_size, padding — параметры conv слоев внутри блока
     '''
     block = nn.Sequential(
-        #ВАШ КОД ЗДЕСЬ
+        nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, padding=padding),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=kernel_size, stride=2, padding=padding)
     )
 
     return block
 
 def decoder_block(in_channels, out_channels, kernel_size, padding):
     '''
-    блок, который принимает на вход карты активации с количеством каналов in_channels, 
+    блок, который принимает на вход карты активации с количеством каналов in_channels,
     и выдает на выход карты активации с количеством каналов out_channels
     kernel_size, padding — параметры conv слоев внутри блока
     '''
     block = nn.Sequential(
-        #ВАШ КОД ЗДЕСЬ
+        nn.ConvTranspose2d(in_channels = in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=2, padding=padding, output_padding=1),
+        nn.BatchNorm2d(out_channels),
+        nn.ReLU(),
     )
 
     return block
@@ -34,26 +39,39 @@ class Autoencoder(nn.Module):
 
         # добавьте несколько слоев encoder block
         # это блоки-составляющие энкодер-части сети
+        # В ячейке ниже приводится проверка построенной архитектуры.
+        # Подумайте, сколько encoder_block и с какими размерами стоит выбрать для корректного решения
         self.encoder = nn.Sequential(
-            #ВАШ КОД ЗДЕСЬ
+            encoder_block(3, 32, 3, 1),
+            encoder_block(32, 64, 3, 1),
+            encoder_block(64, 128, 3, 1),
+            encoder_block(128, 256, 3, 1),
+            encoder_block(256, 512, 3, 1)
         )
 
         # добавьте несколько слоев decoder block
         # это блоки-составляющие декодер-части сети
+        # Не забывайте по необходимости делать ConvTranspose2d и вызов Sigmoid в конце для объединения всех восстановленных изображений и нормализации выходов
+        # В ячейке ниже приводится проверка построенной архитектуры.
+        # Подумайте, сколько decoder_block и с какими размерами стоит выбрать для корректного решения
         self.decoder = nn.Sequential(
-            #ВАШ КОД ЗДЕСЬ
+            decoder_block(512,256,3,1),
+            decoder_block(256,128,3,1),
+            decoder_block(128,64,3,1),
+            decoder_block(64,32,3,1),
+            decoder_block(32,3,3,1),
+            nn.Sigmoid()
         )
 
     def forward(self, x):
 
-        # downsampling 
+        # downsampling
         latent = self.encoder(x)
 
         # upsampling
         reconstruction = self.decoder(latent)
 
         return reconstruction
-
 
 
 def create_model():
